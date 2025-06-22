33bb4fe1e6ba0dd168ffcba1327e935c43f2de4f
DheMasker <104171866+DheMasker@users.noreply.github.com>
2025-06-22 20:55:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 
 # Daftar sumber langganan
 SUB_LINKS = [ 
-    "https://raw.githubusercontent.com/sevcator/5ubscrpt10n/refs/heads/main/full/5ubscrpt10n-b64.txt"
+    "https://raw.githubusercontent.com/Epodonios/v2ray-configs/refs/heads/main/All_Configs_Sub.txt"
 ]
 
 BUGCDN = "104.22.5.240"
