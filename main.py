3eb93c3a0812b0c4e56384eccd2327ef8be638c4
DheMasker <104171866+DheMasker@users.noreply.github.com>
2025-06-22 10:34:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import urllib.parse  # Untuk dekoding
 
 # Daftar sumber langganan
 SUB_LINKS = [ 
-    "https://raw.githubusercontent.com/sevcator/5ubscrpt10n/refs/heads/main/full/5ubscrpt10n-b64.txt"
+    "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/trojan"
 ]
 
 def ambil_langganan():
