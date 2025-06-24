9a0d7b1d1907cba20f1a1fa233b4cdb14ef8d645
DheMasker <104171866+DheMasker@users.noreply.github.com>
2025-06-24 08:34:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import urllib.parse
 
 # Daftar sumber langganan
 SUB_LINKS = [ 
-   
-"https://raw.githubusercontent.com/sevcator/5ubscrpt10n/refs/heads/main/full/5ubscrpt10n-b64.txt"
+    "https://raw.githubusercontent.com/sevcator/5ubscrpt10n/refs/heads/main/full/5ubscrpt10n-b64.txt"
 ]
 
 BUGCDN = "104.22.5.240"
